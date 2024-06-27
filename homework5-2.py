97ddaf69ef5ab95c810408d4c331adade9d89d69
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-06-27 22:45:25 +0300

    Add files via upload

---
+++ homework5-2.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (self.name, self.number_of_floors)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(str(f'Название: {self.name(h1)}, кол-во этажей: {self.number_of_floors(h1)}'))
+print(str(f'Название: {self.name(h2)}, кол-во этажей: {self.number_of_floors(h2)}'))
+print(len(h1))
+print(len(h2))
